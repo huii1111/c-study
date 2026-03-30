66e3ae150d282dca26647051d6b6a0ad8f8b89da
huii1111 <seohuisophia@gmail.com>
2026-03-30 14:51:14 +0900

    배열 연습

---
+++ array/char-array-1.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    char str[30] = "C language";
+    int i = 0;
+
+    while (str[i] != '\0') {
+        i++;
+    }
+
+    printf("문자열 \"%s\"의 길이는 %d입니다.\n", str, i);
+
+    return 0;
+}
