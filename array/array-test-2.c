5f750bbbbb2bc963a70a8677c10d066245384f18
huii1111 <seohuisophia@gmail.com>
2026-03-30 15:47:27 +0900

    배열 교환 실습

---
+++ array/array-test-2.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+int main(void)
+{
+	char s[] = "ABCD";
+	int a = 0, b = 3;
+	char t;
+	while (a < b) //0 3번째 교환-> 1 2  번쨰 교환
+	{
+		t = s[a];
+		s[a] = s[b];
+		s[b] = t;
+		a++;
+		b--;
+
+	}
+
+	printf("%s", s);
+	return 0;
+}
