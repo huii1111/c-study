4b786b516d698c217885da9d4840d6bf0e22917d
huii1111 <seohuisophia@gmail.com>
2026-04-02 12:49:53 +0900

    주석 추가

--- self-learning/function.c
+++ self-learning/function.c
@@ -4,7 +4,11 @@ int digit(int n) { //정수 n을 받아서 계산
 
 	if (n < 10) return n; //만약 n이 10보다 작으면 반환
 	return(n % 10) + digit(n / 10); //n을 10으로 나눈 나머지+ n을 10으로 나눈 값
-}
+} /* (235%10)+digit(235/10)
+	 5+digit(23)
+	 (23%10)+digit(23/10)
+	 3+digit(3)
+	 5 + 3 + 2 = 10 */
 
 int main() {
 
